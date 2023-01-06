0d3bbff467bfd74245681d75356fc5d30cdfc867
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-01-06 19:31:14 +0530

    ll

---
+++ Linkesd List/Nth_node_from_last.cpp
@@ -0,0 +1,26 @@
+int getNthFromLast(Node *head, int n)
+{
+    // Your code here
+    if (head == NULL)
+        return -1;
+    int cnt = 0;
+
+    Node *temp = head;
+    while (temp)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    if (cnt < n)
+        return -1;
+
+    int i = 0;
+    temp = head;
+    while (i < cnt - n)
+    {
+        i++;
+        temp = temp->next;
+    }
+    return temp->data;
+}
