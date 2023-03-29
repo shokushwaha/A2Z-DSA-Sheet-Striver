da9a9afdf625f41107c981859f11f78313244b55
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-03-29 11:06:13 +0530

    dp

--- Dynamic Programming/Strings/Length_of_longest_common_subsequence.cpp
+++ Dynamic Programming/Strings/Length_of_longest_common_subsequence.cpp
@@ -43,3 +43,32 @@ int getLengthOfLCS(string &str1, string &str2)
 
     return solve(a - 1, b - 1, str1, str2, dp);
 }
+
+// dp - tabulation
+int lcs(int x, int y, string s1, string s2)
+{
+    int a = s1.size();
+    int b = s2.size();
+
+    int dp[a + 1][b + 1];
+
+    for (int i = 0; i <= a; i++)
+        dp[i][0] = 0;
+
+    for (int i = 0; i <= b; i++)
+        dp[0][i] = 0;
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= b; j++)
+        {
+            dp[i][j] = 0;
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[a][b];
+}
